5cf090779b12b7ba4c93e3f723030709a17c3f9e
siddardha reddy pasham <178729877+sid1114911149@users.noreply.github.com>
2026-02-08 00:09:00 +0530

    second

--- routes/firmRoutes.js
+++ routes/firmRoutes.js
@@ -1,23 +1,28 @@
-const express=require('express');
-const router=express.Router();
-const firmController=require('../controllers/firmController');
-const verifyToken=require('../middlewares/verifyToken');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
-router.post('/add-firm',verifyToken,firmController.addFirm);
+const firmController = require('../controllers/firmController');
+const verifyToken = require('../middlewares/verifyToken');
+
+router.post(
+  '/add-firm',
+  verifyToken,
+  ...firmController.addFirm
+);
 
 router.get('/uploads/:imageName', (req, res) => {
-    try {
-        const imageName = req.params.imageName;
-
-        const imagePath = path.join(__dirname, '..', 'uploads', imageName);
-
-        res.setHeader('Content-Type', 'image/jpeg'); // or png
-        res.sendFile(imagePath);
-    } catch (error) {
-        res.status(500).json({ message: 'Image not found' });
-    }
+  try {
+    const imageName = req.params.imageName;
+    const imagePath = path.join(__dirname, '..', 'uploads', imageName);
+    res.sendFile(imagePath);
+  } catch (error) {
+    res.status(500).json({ message: 'Image not found' });
+  }
 });
-router.get('/get-allFirms',verifyToken,firmController.getAllFirms)
-router.delete('/delete/:firmId',firmController.deleteFirm);
 
-module.exports=router;
+router.get('/get-allFirms', verifyToken, firmController.getAllFirms);
+
+router.delete('/delete/:firmId', verifyToken, firmController.deleteFirm);
+
+module.exports = router;
